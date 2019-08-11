9ae17197e5f78479733da871b7574e075d78e31c
alef1 <alef1@LAPTOP-OFIJ2E99>
2019-08-11 01:11:26 -0400

    excel

--- POMDesignConcept/src/main/java/com/qa/crm/pages/ContactsPage.java
+++ POMDesignConcept/src/main/java/com/qa/crm/pages/ContactsPage.java
@@ -2,8 +2,10 @@ package com.qa.crm.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import com.qa.crm.base.TestBase;
 
@@ -14,6 +16,23 @@ public class ContactsPage extends TestBase{
 	
 	@FindBy(xpath="//td[contains(text(),'Contacts')]")
 	WebElement Contactlebel;
+	@FindBy(xpath="//*[@name='title']")
+	WebElement dropdown;
+	
+	@FindBy(xpath="//*[@name='first_name']")
+	WebElement firstname;
+	
+	@FindBy(xpath="//*[@name='surname']")
+	WebElement lastname;
+	
+	@FindBy(xpath="//*[@name='client_lookup']")
+	WebElement company;
+	
+	@FindBy(xpath="//*[@id=\"contactForm\"]/table/tbody/tr[1]/td/input[2]  ")
+	WebElement saveBtn;
+	
+	
+	
 	
 	public ContactsPage() {
 		PageFactory.initElements(driver, this);
@@ -29,6 +48,20 @@ public class ContactsPage extends TestBase{
 	
 	public void clickOnContact() {
 		driver.findElement(By.xpath("//input[@name='contact_id' and @value ='52714427']")).click();
+	}
+	
+	public void createNewContact(String title, String ftname, String ltname, String com ) {
+		Select select = new Select(dropdown);
+		select.selectByVisibleText(title);
+		firstname.sendKeys(ftname);
+		lastname.sendKeys(ltname);
+		company.sendKeys(com);
+		//Actions action = new Actions(driver);
+		//action.moveToElement(saveBtn).click().build().perform();
+		//driver.switchTo().frame("mainpanel");
+		saveBtn.click();
+		
+		
 	}
    
 }
